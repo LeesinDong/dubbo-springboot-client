c1ebb1eddf784f80e4616bc6b37ccd190c778fcb
dataiyangu <32248827@qq.com>
2020-04-19 23:15:40 +0800

    init

---
+++ src/test/java/com/leesin/dubbospringbootclient/DubboSpringbootClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leesin.dubbospringbootclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DubboSpringbootClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
